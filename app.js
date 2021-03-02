b9c15033fddf4f0cbd07e9f239af2ba856654f2c
Deepakshi <deepakshisud@gmail.com>
2021-03-02 12:30:17 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -70,6 +70,8 @@ app.put('/place', async(req, res) => {
     res.render('place', {place});
 })
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Listening");
 })
